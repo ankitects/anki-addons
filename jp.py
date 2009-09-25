f44d233090038ddc5bc1b9f15fc7e0558156064a
Damien Elmes <git@ichi2.net>
2009-09-26 06:33:35 +0900

    delay init of jp plugin so we don't leak module fds

--- jp.py
+++ jp.py
@@ -2,12 +2,16 @@
 # Copyright: Damien Elmes <anki@ichi2.net>
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 
-import japanese.model
-import japanese.furigana
-import japanese.reading
-import japanese.lookup
-import japanese.stats
-import japanese.bulkreading
+def init():
+    import japanese.model
+    import japanese.furigana
+    import japanese.reading
+    import japanese.lookup
+    import japanese.stats
+    import japanese.bulkreading
 
 from ankiqt import mw
 mw.registerPlugin("Japanese Support", 172)
+
+from anki.hooks import addHook
+addHook('init', init)
