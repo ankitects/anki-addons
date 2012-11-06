57d0e21f984b03d265ee9da4b22209c6a63e2b5f
Damien Elmes <git@ichi2.net>
2012-11-07 03:54:32 +0900

    update changekeys

--- changekeys.py
+++ changekeys.py
@@ -1,26 +1,30 @@
 # -*- coding: utf-8 -*-
 # Copyright: Damien Elmes <anki@ichi2.net>
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
+#
+# An example of how to override review shortcuts. Changes:
+# - when '5' is pressed with the question shown, show the answer
+# - when '6' is pressed with the answer shown, answer with the default button
+# - when '7' is pressed, undo the last review
+# - when '8' is pressed, mark the card
+# You can edit the code below to customize it.
 
-# this plugin allows you to redefine the keys used to answer cards. it defines
-# 'x' as a failed answer and 'y' as a correct answer.
+from aqt import mw
+from aqt.reviewer import Reviewer
+from anki.hooks import wrap
 
-# version 1: initial release
+def keyHandler(self, evt, _old):
+    key = unicode(evt.text())
+    if key == "5" and self.state == "question":
+        self._showAnswerHack()
+    elif key == "6" and self.state == "answer":
+        self._answerCard(self._defaultEase())
+    elif key == "7":
+        self.mw.onUndo()
+    elif key == "8":
+        self.onMark()
+    else:
+        return _old(self, evt)
 
-from ankiqt import mw
+Reviewer._keyHandler = wrap(Reviewer._keyHandler, keyHandler, "around")
 
-def newEventHandler(evt):
-    if mw.state == "showAnswer":
-        key = unicode(evt.text())
-        if key == "x":
-            evt.accept()
-            return mw.cardAnswered(1)
-        if key == "y":
-            evt.accept()
-            return mw.cardAnswered(4)
-    return oldEventHandler(evt)
-
-oldEventHandler = mw.keyPressEvent
-mw.keyPressEvent = newEventHandler
-
-mw.registerPlugin("Change Keys", 7)
