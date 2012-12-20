2204f43bb9d3df0f05e609e3efefa5528ecebbd9
Damien Elmes <git@ichi2.net>
2012-12-20 21:26:52 +0900

    fix nodefaultanswer plugin

--- nodefaultanswer.py
+++ nodefaultanswer.py
@@ -6,10 +6,22 @@
 # answer buttons.
 #
 
+from aqt.qt import *
 from aqt import mw
-from anki.hooks import addHook
+import aqt.reviewer
+from anki.hooks import addHook, wrap
 
 def noAnswer():
     mw.reviewer.web.setFocus()
 
 addHook("showAnswer", noAnswer)
+
+def keyHandler(self, evt, _old):
+    key = unicode(evt.text())
+    if (key == " " or evt.key() in (Qt.Key_Return, Qt.Key_Enter)):
+        if self.state == "answer":
+            return
+    _old(self, evt)
+
+aqt.reviewer.Reviewer._keyHandler = wrap(
+    aqt.reviewer.Reviewer._keyHandler, keyHandler, "around")
