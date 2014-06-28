8295ef01f09b0ab895574e0debcace76081950c4
Damien Elmes <git@ichi2.net>
2014-06-29 05:12:39 +0900

    add warning about changing settings

--- japanese/notetypes.py
+++ japanese/notetypes.py
@@ -3,7 +3,14 @@
 Author: RawToast
 License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 
-Configure settings for the note types and source fields for the Japanese Support plugin here
+Configure settings for the note types and source fields for the Japanese
+Support plugin here
+
+WARNING: By default, this add-on operates on note types that contain the word
+"Japanese" in their name. You can alter this by changing the settings below,
+but please be aware that this will break things if you share your deck with
+others. So please only consider changing these options if you're not planning
+to share your deck.
 """
 
 # If sets to True the plugin will work for ALL note types.
