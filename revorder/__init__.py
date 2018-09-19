184c662487e3615011270e4d394ee785a9a2ee98
xquercus <jeff@mossycup.com>
2018-09-19 10:37:07 -0400

    Patch in original _fillRev

--- revorder/__init__.py
+++ revorder/__init__.py
@@ -17,6 +17,11 @@ order = "ivl desc"
 
 from anki.sched import Scheduler
 
+import random
+import anki
+from anki.utils import ids2str
+from anki.schedv2 import Scheduler
+
 def _fillRev(self):
     if self._revQueue:
         return True
@@ -47,4 +52,39 @@ did = ? and queue = 2 and due <= ?"""
         # nothing left in the deck; move to next
         self._revDids.pop(0)
 
-Scheduler._fillRev = _fillRev
+anki.sched.Scheduler._fillRev = _fillRev
+
+def _v2_fillRev(self):
+    if self._revQueue:
+        return True
+    if not self.revCount:
+        return False
+
+    lim = min(self.queueLimit, self._currentRevLimit())
+    if lim:
+        self._revQueue = self.col.db.list("""
+select id from cards where
+did in %s and queue = 2 and due <= ?
+order by due
+limit ?""" % (ids2str(self.col.decks.active())),
+                self.today, lim)
+
+        if self._revQueue:
+            if self.col.decks.get(self.col.decks.selected(), default=False)['dyn']:
+                # dynamic decks need due order preserved
+                self._revQueue.reverse()
+            else:
+                # fixme: as soon as a card is answered, this is no longer consistent
+                r = random.Random()
+                r.seed(self.today)
+                r.shuffle(self._revQueue)
+            return True
+
+    if self.revCount:
+        # if we didn't get a card but the count is non-zero,
+        # we need to check again for any cards that were
+        # removed from the queue but not buried
+        self._resetRev()
+        return self._fillRev()
+
+anki.schedv2.Scheduler._fillRev = _v2_fillRev
