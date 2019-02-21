3f12c89315ac7a0e940754b501d0eb5910d32ae0
Damien Elmes <gpg@ankiweb.net>
2019-02-21 16:36:39 +1000

    util for fixing fields with unbalanced html

---
+++ fixinvalidhtml/__init__.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+# Copyright: Ankitects Pty Ltd and contributors
+# License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
+#
+# Feed field HTML through BeautifulSoup to fix things like unbalanced div tags.
+#
+
+from aqt import mw
+from aqt.qt import *
+from anki.hooks import addHook
+from aqt.utils import showInfo
+
+from bs4 import BeautifulSoup
+
+def onFixHTML(browser):
+    nids = browser.selectedNotes()
+    if not nids:
+        showInfo("Please select some notes.")
+        return
+
+    mw.checkpoint("Fix Invalid HTML")
+
+    mw.progress.start(immediate=True)
+    try:
+        changed = _onFixHTML(browser, nids)
+    finally:
+        mw.progress.finish()
+
+    browser.model.reset()
+    mw.requireReset()
+
+    showInfo("Updated %d/%d notes." % (changed, len(nids)), parent=browser)
+
+def _onFixHTML(browser, nids):
+    changed = 0
+    for c, nid in enumerate(nids):
+        note = mw.col.getNote(nid)
+        if _fixNoteHTML(note):
+            changed += 1
+        mw.progress.update(label="Processed %d/%d notes" % (c+1, len(nids)))
+    return changed
+
+# true on change
+def _fixNoteHTML(note):
+    changed = False
+    for fld, val in note.items():
+        parsed = str(BeautifulSoup(val, "html.parser"))
+        if parsed != val:
+            note[fld] = parsed
+            changed = True
+
+    if changed:
+        note.flush()
+
+    return changed
+
+def onMenuSetup(browser):
+    act = QAction(browser)
+    act.setText("Fix Invalid HTML")
+    mn = browser.form.menu_Notes
+    mn.addSeparator()
+    mn.addAction(act)
+    act.triggered.connect(lambda b=browser: onFixHTML(browser))
+
+addHook("browser.setupMenus", onMenuSetup)
