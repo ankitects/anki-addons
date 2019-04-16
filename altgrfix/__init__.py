b806bb54af6347009c91d75dfe11a580eef94f55
Damien Elmes <gpg@ankiweb.net>
2019-04-16 14:26:52 +1000

    alt+gr fix

---
+++ altgrfix/__init__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
+
+from aqt.qt import *
+
+def _runJavaScriptSync(page, js, timeout=500):
+    result = None
+    eventLoop = QEventLoop()
+    called = False
+
+    def callback(val):
+        nonlocal result, called
+        result = val
+        called = True
+        eventLoop.quit()
+
+    page.runJavaScript(js, callback)
+
+    if not called:
+        timer = QTimer()
+        timer.setSingleShot(True)
+        timer.timeout.connect(eventLoop.quit)
+        timer.start(timeout)
+        eventLoop.exec_()
+
+    if not called:
+        print('runJavaScriptSync() timed out')
+    return result
+
+
+def event(self, evt):
+    if evt.type() == QEvent.ShortcutOverride:
+        # alt-gr bug workaround
+        exceptChars = (str(num) for num in range(1, 10))
+        if evt.text() not in exceptChars:
+            js = '''
+var e=document.activeElement;
+(e.tagName === "DIV" && e.contentEditable) ||
+["INPUT", "TEXTAREA"].indexOf(document.activeElement.tagName) !== -1'''
+            if _runJavaScriptSync(self.page(), js, timeout=100):
+                evt.accept()
+                return True
+    return QWebEngineView.event(self, evt)
+
+from aqt.webview import AnkiWebView
+AnkiWebView.event = event
