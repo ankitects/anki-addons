6f279e081d04950138db84f79c93ffe2810906ad
Damien Elmes <gpg@ankiweb.net>
2020-01-15 14:47:06 +1000

    black on build.py; fix linting and run mypy

--- build.py
+++ build.py
@@ -5,6 +5,7 @@ import subprocess
 
 build_dir = "_build"
 
+
 def addons():
     dirs = []
     for file in os.listdir("."):
@@ -12,6 +13,7 @@ def addons():
         if os.path.exists(init):
             dirs.append(file)
     return dirs
+
 
 def build_all():
     needed = []
@@ -26,45 +28,55 @@ def build_all():
             print("building", dir, "...")
             build(dir)
 
+
 def needs_build(dir):
     build_ts = last_build_time(dir)
     mod_ts = most_recent_change(dir)
     return mod_ts > build_ts
+
 
 def build(dir):
     out = target_file(dir)
     if os.path.exists(out):
         os.unlink(out)
     ensure_manifest(dir)
-    subprocess.check_call(["7z", "a", "-tzip",
-                           "-x!meta.json",
-                           "-x!tests",
-                           "-bso0", # less verbose
-                           out,
-                           # package folder contents but not folder itself
-                           "-w", os.path.join(dir, ".")])
+    subprocess.check_call(
+        [
+            "7z",
+            "a",
+            "-tzip",
+            "-x!meta.json",
+            "-x!tests",
+            "-bso0",  # less verbose
+            out,
+            # package folder contents but not folder itself
+            "-w",
+            os.path.join(dir, "."),
+        ]
+    )
+
 
 def lint(dirs):
-    run([
-        "pylint",
-        "-j", "0",
-        "--rcfile=../dtop/.pylintrc",
-        "-f", "colorized",
-        "--extension-pkg-whitelist=PyQt5",
-    ] + dirs)
+    run("../dtop/pyenv/bin/mypy " + " ".join(dirs))
+    run(
+        "../dtop/pyenv/bin/pylint -j 0 --rcfile=../dtop/pylib/.pylintrc -f colorized --extension-pkg-whitelist=ankirspy,PyQt5 "
+        + " ".join(dirs)
+    )
+
 
 def run(cmd):
-    env=os.environ.copy()
-    env["PYTHONPATH"]="../dtop"
-    subprocess.check_call(cmd, env=env)
+    subprocess.check_call(cmd, shell=True)
+
 
 def ensure_manifest(dir):
     manifest_path = os.path.join(dir, "manifest.json")
     if not os.path.exists(manifest_path):
         open(manifest_path, "w").write(json.dumps(dict(package=dir, name=dir)))
 
+
 def target_file(dir):
-    return os.path.join(build_dir, dir+".ankiaddon")
+    return os.path.join(build_dir, dir + ".ankiaddon")
+
 
 def last_build_time(dir):
     out = target_file(dir)
@@ -72,6 +84,7 @@ def last_build_time(dir):
         return os.stat(out).st_mtime
     except:
         return 0
+
 
 def most_recent_change(dir):
     newest = 0
@@ -82,6 +95,7 @@ def most_recent_change(dir):
             newest = max(newest, os.stat(path).st_mtime)
 
     return newest
+
 
 build_all()
 print("all done")
