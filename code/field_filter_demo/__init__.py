12341feba0dad525fe7be19bac539a1622471c53
Damien Elmes <gpg@ankiweb.net>
2020-01-17 09:37:33 +1000

    update for latest template changes

--- code/field_filter_demo/__init__.py
+++ code/field_filter_demo/__init__.py
@@ -2,21 +2,34 @@
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
 """
-An example of how you can use the hooks in Anki 2.1.20 to modify the
-way Anki renders cards.
+An example of how you can add content to a card's question and answer
+dynamically in Anki 2.1.20.
 
-When Anki encounters an unknown field in the template, it shows the message
-"{unknown field <name>}". We want to avoid that message when the card is studied
-on other platforms where the add-on is not installed. To accomplish this, we
-can use an empty field name in the template replacement, eg instead of
-{{myfilter:field}}, we'll use {{myfilter:}}, and the filter will be passed
-a blank string. On platforms that don't have the filter, it will not add
-anything to the template.
+One way to accomplish this would be to automatically insert a
+"GeneratedField" field into the list of fields when a card is being
+rendered. The user could then use {{GeneratedField}} on their card template
+to show the extra information. But this is not a great solution, as when
+the user switches to a mobile devices or a device without the add-on
+installed, they'd get an error message that the field doesn't exist.
+Another downside of that approach is that if you're creating hundreds
+of fields, you may be doing extra work that does not end up getting used.
 
-Because the filter code will only run when the filter is specified on the
-template, the filter can potentially provide hundreds of options without
-having to calculate them all up front and insert them into the list of fields.
-Instead, it can just calculate the required information on demand.
+A better approach is to leverage Anki's field filter system. If the user
+places {{myfilter:a_valid_field}} on their template, the field_filter hook
+will be called, allowing your add-on to modify or add to the text contained
+in a_valid_field.
+
+While filters can be used to change the text of fields, in this case we are
+only interested in adding new content, so we don't need to use the contents
+of a_valid_field. While you could ask your user to create a blank field for
+the benefit of your add-on so they don't get an invalid field message,
+Anki provides a shortcut - if the field reference ends with a :, it will
+not display the invalid field message, and will pass a blank string to the
+filter. So users can just use {{myfilter:}} on the template instead.
+
+On devices that are not running the add-on, unrecognized filters will be
+silently ignored, so on a stock install {{myfilter:}} will not add anything to
+the template.
 
 We'll make an add-on that allows the user to add the following to their
 template:
@@ -24,59 +37,18 @@ template:
 {{info-card-interval:}}   -- shows the card's interval
 {{info-note-creation:}}   -- shows the date the note was created
 
-The filter hook is not provided with a card or note ID, but there is
-another hook we can use to get them. We could just store the IDs, and then
-fetch the card or note from the database each time a filter is encountered.
-But if the user adds tens or hundreds of these replacements to their card,
-fetching the same card or note over and over again might be slow.
-
-So instead, we store the current card and note at the start of the
-rendering process, and each time a filter is processed on the card,
-the objects can be reused.
+We make use of the context argument to gain access to the card and note
+that is being rendered. See template.py for the other options it provides.
 """
 
 import time
-from typing import Dict, Optional, Tuple
 
 from anki import hooks
-from anki.cards import Card
-from anki.notes import Note
-from anki.types import NoteType, QAData
-from aqt import mw
-
-# we will update these each time a new card is rendered
-current_card: Optional[Card] = None
-current_note: Optional[Note] = None
-
-# called when a card is going to be rendered. we can update
-# the current card and note here for the filter below to use.
-def my_card_will_render_func(
-    templates: Tuple[str, str], fields: Dict[str, str], notetype: NoteType, data: QAData
-) -> Tuple[str, str]:
-    # tell python we want to update the above global variables
-    global current_card, current_note
-
-    # get the IDs stored in the QAData object (see types.py)
-    card_id = data[0]
-    note_id = data[1]
-
-    # when the user is previewing cards in the add screen, the card
-    # won't have been added to the database yet. so we try to fetch
-    # the card, and set it to None if the fetch fails
-    try:
-        current_card = mw.col.getCard(card_id)
-        current_note = mw.col.getNote(note_id)
-    except:
-        current_card = None
-        current_note = None
-
-    # this is a filter, so it must return its first argument.
-    return templates
-
+from anki.template import TemplateRenderContext
 
 # called each time a custom filter is encountered
 def my_field_filter(
-    field_text: str, field_name: str, filter_name: str, fields: Dict[str, str],
+    field_text: str, field_name: str, filter_name: str, context: TemplateRenderContext,
 ) -> str:
     if not filter_name.startswith("info-"):
         # not our filter, return string unchanged
@@ -88,15 +60,15 @@ def my_field_filter(
     except ValueError:
         return invalid_name(filter_name)
 
-    # if in the adding screen, we can't get info about the card
-    if not current_card:
+    # if in the adding screen, the card hasn't been saved to DB yet
+    if rest.startswith("card-") and not context.card():
         return "(n/a)"
 
     # call the appropriate function
     if rest == "card-interval":
-        return card_interval()
+        return card_interval(context)
     elif rest == "note-creation":
-        return note_creation()
+        return note_creation(context)
     else:
         return invalid_name(filter_name)
 
@@ -105,17 +77,15 @@ def invalid_name(filter_name: str) -> str:
     return f"invalid filter name: {filter_name}"
 
 
-def card_interval() -> str:
-    return str(current_card.ivl)
+def card_interval(ctx: TemplateRenderContext) -> str:
+    return str(ctx.card().ivl)
 
 
-def note_creation() -> str:
+def note_creation(ctx: TemplateRenderContext) -> str:
     # convert millisecond timestamp to seconds
-    note_creation_unix_timestamp = current_note.id // 1000
+    note_creation_unix_timestamp = ctx.note().id // 1000
     # convert timestamp to a human years-months-days
     return time.strftime("%Y-%m-%d", time.localtime(note_creation_unix_timestamp))
 
-
-# register the hooks we want to use
-hooks.card_will_render.append(my_card_will_render_func)
+# register our function to be called when the hook fires
 hooks.field_filter.append(my_field_filter)
