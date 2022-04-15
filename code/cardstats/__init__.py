edc732ad98f361e0a37c138b329154b45aee23af
Damien Elmes <gpg@ankiweb.net>
2022-04-15 15:23:07 +1000

    Use cardStats JS directly instead of legacy Python routine
    Avoids flicker when transitioning between cards.

--- code/cardstats/__init__.py
+++ code/cardstats/__init__.py
@@ -7,9 +7,8 @@
 #
 
 from anki.hooks import addHook
-from aqt import colors, mw
+from aqt import mw
 from aqt.qt import *
-from aqt.theme import theme_manager
 from aqt.webview import AnkiWebView
 
 
@@ -54,13 +53,13 @@ class CardStats(object):
             self.web = ThinAnkiWebView()
             self.shown = self._addDockable(("Card Info"), self.web)
             self.shown.closed.connect(self._onClosed)
+            self._load_html()
         self._update()
 
     def hide(self):
         if self.shown:
             self._remDockable(self.shown)
             self.shown = None
-            # actionself.mw.form.actionCstats.setChecked(False)
 
     def toggle(self):
         if self.shown:
@@ -75,36 +74,45 @@ class CardStats(object):
     def _update(self):
         if not self.shown:
             return
-        txt = ""
         r = self.mw.reviewer
-        d = self.mw.col
-        if r.card:
-            txt += "<h3>Current</h3>"
-            txt += d.cardStats(r.card)
+        id = r.card.id if r.card else "null"
+        self.web.eval(
+            f"current.then(s => s.$set({{ cardId: {id}, includeRevlog: false }}));"
+        )
         lc = r.lastCard()
-        if lc:
-            txt += "<h3>Last</h3>"
-            txt += d.cardStats(lc)
-        if not txt:
-            txt = "No current card or last card."
-        style = self._style()
+        id = lc.id if lc else "null"
+        self.web.eval(
+            f"previous.then(s => s.$set({{ cardId: {id}, includeRevlog: false }}));"
+        )
+
+    def _load_html(self):
         self.web.setHtml(
             """
 <html><head>
+<script src="js/vendor/bootstrap.bundle.min.js"></script>
+<link href="pages/card-info-base.css" rel="stylesheet" />
+<link href="pages/card-info.css" rel="stylesheet" />
 </head><style>%s</style>
-<body><center>%s</center></body></html>"""
-            % (style, txt)
+<body>
+<script src="pages/card-info.js"></script>
+<center>
+<h3>Current</h3>
+<div id=current></div>
+<h3>Previous</h3>
+<div id=previous></div>
+<script>
+const current = anki.setupCardInfo(document.getElementById("current"));
+const previous = anki.setupCardInfo(document.getElementById("previous"));
+</script>
+</center></body></html>"""
+            % self._style()
         )
+        self.web.on_theme_did_change()
 
     def _style(self):
-        return f"""
-td {{
-    font-size: 80%; }}
-body {{
-    background: {theme_manager.color(colors.WINDOW_BG)} }}
-td, h3, center {{ 
-    color: {theme_manager.color(colors.TEXT_FG)}
-}}
+        return """
+td { font-size: 80%; }
+.card-info-placeholder { position: relative !important; }
 """
 
 
