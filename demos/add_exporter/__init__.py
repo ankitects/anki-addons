3c6b8a118a62bd61fa788bc3ce86b55efc14a00e
RumovZ <gp5glkw78@relay.firefox.com>
2024-04-28 21:37:10 +0200

    Add demo add-on for adding a custom exporter

---
+++ demos/add_exporter/__init__.py
@@ -0,0 +1,15 @@
+"""
+An example of extending the deck options screen with raw HTML/JS.
+"""
+
+from pathlib import Path
+
+from aqt import gui_hooks
+from aqt.webview import AnkiWebView
+
+
+def setup(webview: AnkiWebView) -> None:
+    webview.eval(Path(__file__).with_name("init.js").read_text(encoding="utf8"))
+
+
+gui_hooks.webview_did_inject_style_into_page.append(setup)
